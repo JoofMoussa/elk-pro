ae659ded8504745d74c2b33c32603c07f616e08b
XpertE0! <dioufm65@yahoo.com>
2023-08-17 13:40:39 +0000

    mise a jour index;js

--- index.js
+++ index.js
@@ -6,39 +6,20 @@ const esUrl = "http://localhost:9200/";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//insertion des donnees avec Bulk : fichier data.json
-
-app.post("/data", async(req, res) => {
-    try {
-        const mesDonnees = require("data.json");
-        const tailleDonnees = mesDonnees.length;
-        for (let idx = 0; idx < tailleDonnees; idx++) { // 
-            await axios.post(`${esUrl}${req.body.index}/_doc`, mesDonnees[idx]);
-        }
-
-        res.json("**** Donnees Inserees ****");
-    } catch (error) {
-        res.status(500).json(error);
-    }
-});
-
-
+const cors = require('cors');
+app.use(cors());
 
 
 app.post("/Create", async(req, res) => {
     try {
-        const verifierIndex = () =>
-            new Promise((resolve) => {
-                axios
-                    .get(`${esUrl}${req.body.index}`)
-                    .then((_) => {
-                        resolve(true);
-                    })
-                    .catch(() => {
-                        resolve(false);
-                    });
-            });
+        const verifierIndex = async() => {
+            try {
+                await axios.head(`${esUrl}${req.body.index}`);
+                return true;
+            } catch (error) {
+                return false;
+            }
+        };
 
         const siIndexExiste = await verifierIndex();
         if (!siIndexExiste) {
@@ -91,8 +72,23 @@ app.post("/Create", async(req, res) => {
     }
 });
 
-// Recuperer les donnees
+// Insertion des donnees avec Bulk: fichier data.json
+app.post("/data", async(req, res) => {
+    try {
+        const mesDonnees = require("./data.json");
+        const tailleDonnees = mesDonnees.length;
+        for (let idx = 0; idx < tailleDonnees; idx++) {
+            await axios.post(`${esUrl}${req.body.index}/_doc`, mesDonnees[idx]);
+        }
 
+        res.json("**** Donnees Inserees ****");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+// // Recuperer les donnees
 app.get("/Read/:index/:id", async(req, res) => {
     try {
         const esResponse = await axios.get(`${esUrl}${req.params.index}/_doc/${req.params.id}`);
@@ -102,7 +98,7 @@ app.get("/Read/:index/:id", async(req, res) => {
     }
 });
 
-// Mis a jour des donnees
+// // Mis a jour des donnees
 app.put("/Update/:index/:id", async(req, res) => {
     try {
         const esResponse = await axios.put(`${esUrl}${req.params.index}/_doc/${req.params.id}`, req.body);
@@ -122,12 +118,69 @@ app.delete("/Delete/:index/:id", async(req, res) => {
     }
 });
 
+
+
+app.get("/data/:index", async(req, res) => {
+    try {
+        let response;
+        const test = req.query.test;
+
+        switch (test) {
+            case "sorting":
+                response = await axios.post(`${esUrl}${req.params.index}/_search`, {
+                    sort: {
+                        inscription: "desc",
+                    },
+                });
+                break;
+
+            case "matching":
+                response = await axios.post(`${esUrl}${req.params.index}/_search`, {
+                    query: {
+                        match: {
+                            pays: "samoa",
+                        },
+                    },
+                });
+                break;
+
+            case "multi-matching":
+                response = await axios.post(`${esUrl}${req.params.index}/_search`, {
+                    query: {
+                        bool: {
+                            must: [{
+                                    match: {
+                                        nom: "Anastacio Stamm",
+                                    },
+                                },
+                                {
+                                    match: {
+                                        pays: "Samoa",
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                });
+                break;
+
+            default:
+                response = await axios.get(`${esUrl}${req.params.index}/_search`);
+                break;
+        }
+
+        res.json(response.data);
+    } catch (error) {
+        res.json(error);
+    }
+});
+// Voir si l index existe
+// curl -I "localhost:9200/my-data-stream?pretty"
 app.get("/", (req, res) => {
     res.send("Serveur en marche!");
 });
 
-
-//Lancement du serveur
+// Lancement du serveur
 app.listen(3003, () => {
     console.log('Serveur en écoute sur le port : http://localhost:3003/');
 });
